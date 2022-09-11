3fa8bd172ea0189719d42e8fdca096f657c85e1d
unknown <kmafh>
2022-09-11 21:31:37 +0200

    Trabajando en Respuestas. Pendiente de Component ands Services. V 55.

---
+++ Microservices/respuesta/src/main/java/com/talan/microservicios/app/respuestas/RespuestaApplication.java
@@ -0,0 +1,19 @@
+package com.talan.microservicios.app.respuestas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@EntityScan({"com.talan.microservicios.app.respuestas.models.entity",
+	"com.tala.microservicios.app.usuarios.models.entity.alumno",
+	"com.talan.microservicios.commons.examenes.models.entity"})
+@SpringBootApplication
+public class RespuestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespuestaApplication.class, args);
+	}
+
+}
